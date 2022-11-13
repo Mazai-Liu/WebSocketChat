26bec2e9ab4b3a4e0a95b7af8437c64782966afc
Mazai-Liu <64641934+Mazai-Liu@users.noreply.github.com>
2022-11-13 20:16:51 +0800

    Update main.js

--- vue-chat/src/main.js
+++ vue-chat/src/main.js
@@ -10,8 +10,8 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 const app = createApp(App);
 app.config.globalProperties.$axios = axios;
 app.config.globalProperties.$router = router;
-app.config.globalProperties.$github_url = "https://cdn.jsdelivr.net/gh/Mazai-Liu/pictures@main/";
-app.config.globalProperties.$api_token = "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";
+app.config.globalProperties.$github_url = "";
+app.config.globalProperties.$api_token = "";
 
 
 app.use(router).use(bootstrap).mount('#app')
